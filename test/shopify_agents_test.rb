0d6ca4631f545c24916e12fd43967920abe0ed25
Jenny Shen <jenny.shen@shopify.com>
2020-02-25 09:19:39 -0500

    Add same site none incompatible

--- test/shopify_agents_test.rb
+++ test/shopify_agents_test.rb
@@ -674,4 +674,37 @@ describe "Shopify agents" do
       name: 'iOS',
     }), sniffer.os_info
   end
+
+  INCOMPATIBLE_USER_AGENTS = [
+    "Mozilla/5.0 (iPhone; CPU iPhone OS 12_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko)"\
+      " GSA/87.0.279142407 Mobile/15E148 Safari/605.1",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/605.1.15 (KHTML, like Gecko)"\
+      " Version/12.1.2 Safari/605.1.15",
+    "Mozilla/5.0 (Linux; U; Android 7.0; en-US; SM-G935F Build/NRD90M) AppleWebKit/534.30 (KHTML, like Gecko) "\
+      "Version/4.0 UCBrowser/11.3.8.976 U3/0.8.0 Mobile Safari/534.30",
+  ]
+
+  INCOMPATIBLE_USER_AGENTS.each do |user_agent|
+    it "user agent #{user_agent} is correctly marked as incompatible" do
+      sniffer = BrowserSniffer.new(user_agent)
+
+      refute sniffer.same_site_none_compatible?
+    end
+  end
+
+  COMPATIBLE_USER_AGENTS = [
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117"\
+      " Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:72.0) Gecko/20100101 Firefox/72.0",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.4"\
+      " Safari/605.1.15",
+  ]
+
+  COMPATIBLE_USER_AGENTS.each do |user_agent|
+    it "user agent #{user_agent} is correctly marked as compatible" do
+      sniffer = BrowserSniffer.new(user_agent)
+      
+      assert sniffer.same_site_none_compatible?
+    end
+  end
 end
